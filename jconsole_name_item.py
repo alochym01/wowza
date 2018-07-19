9296e889358cb3821c496d5e6054bb299acb1ab8
alochym01 <alochym01@gmail.com>
2018-07-19 09:40:38 +0700

    Create jconsole_name_item.py

---
+++ jconsole_name_item.py
@@ -0,0 +1,19 @@
+#https://www.wowza.com/docs/how-to-use-jconsole-with-wowza-media-server#remoteJMXConfigObjectList
+from jmxquery import JMXConnection, JMXQuery
+
+jmx = JMXConnection("service:jmx:rmi://localhost:8084/jndi/rmi://localhost:8085/jmxrmi",jmx_username="admin", jmx_password="admin", java_path="/usr/local/WowzaStreamingEngine/java/bin/java")
+
+mediacache_obj = [
+	"WowzaStreamingEngine:name=Connection*",
+	"WowzaStreamingEngine:name=HandlerThreadPool*",
+	"WowzaStreamingEngine:name=IOPerformance*",
+	"WowzaStreamingEngine:name=Server*",
+	"WowzaStreamingEngine:name=TransportThreadPool*"
+]
+for obj in mediacache_obj:
+    wowza_object = jmx.query([JMXQuery(obj)])
+    for i in wowza_object:
+        print(i.to_string())
+        #print('{0} value is {1}'.format(i.to_query_string(), i.value))
+    print('===============================')
+    print('\n')
