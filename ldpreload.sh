c5710368bc9d321237de7106623e51ce7a8eefae
alochym01 <alochym01@gmail.com>
2018-08-03 16:09:00 +0700

    Create ldpreload.sh

---
+++ ldpreload.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d /Library/WowzaStreamingEngine ]
+then
+  # OS X
+  . /Library/WowzaStreamingEngine/bin/setenv.sh
+else
+  # Linux
+  . /usr/local/WowzaStreamingEngine-4.7.5_01/bin/setenv.sh
+fi
+         
+$_EXECJAVA -Dcom.wowza.wms.native.base="linux" -Dcom.wowza.wms.AppHome="$WMSAPP_HOME" -Dcom.wowza.wms.ConfigURL="$WMSCONFIG_URL" -Dcom.wowza.wms.ConfigHome="$WMSCONFIG_HOME" -cp $WMSAPP_HOME/bin/wms-bootstrap.jar com.wowza.wms.bootstrap.Bootstrap outputLDPRELOAD
